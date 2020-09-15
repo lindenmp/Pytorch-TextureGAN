f9136cec84435a42ac3bdeb5ee43cda77513051f
Linden Parkes <lindenparkes@gmail.com>
2020-09-14 20:20:23 -0400

    minor edits

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ def get_transforms(args):
     transforms_list = [
         custom_transforms.RandomSizedCrop(args.image_size, args.resize_min, args.resize_max),
         custom_transforms.RandomHorizontalFlip(),
+        # custom_transforms.ScaleImg(args.image_size),
+        # custom_transforms.ReturnImg(),
         custom_transforms.toTensor()
     ]
     if args.color_space == 'lab':
